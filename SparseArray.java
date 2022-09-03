033ac0fff9a9afcf368c374463e2df30a1885e26
Tac <188450045@qq.com>
2022-09-03 14:39:52 +0800

    firsttest

---
+++ SparseArray.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+class SparseArray {
+    public int maxProfit(int[] prices) {
+        int index1=0;
+        int index2=prices.length-1;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        while(true){
+            if(prices[index1]<min){
+                min=prices[index1];
+                index1++;
+            }
+            if(index1==index2)break;
+            if(prices[index2]>max){
+                max=prices[index2];
+                index2--;
+            }
+            if(index1==index2)break;
+        }
+        if(max>min)return max=min;
+        else return 0;
+    }
+}
