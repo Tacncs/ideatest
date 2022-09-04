a6e436af3c6b39979c7eaa897feda2059a2a7fa2
Tac <188450045@qq.com>
2022-09-04 16:07:36 +0800

    hotfirst

--- SparseArray.java
+++ SparseArray.java
@@ -2,6 +2,7 @@ package Arrays;
 
 class SparseArray {
     public int maxProfit(int[] prices) {
+        //h1111111
         int index1=0;
         int index2=prices.length-1;
         int max=Integer.MIN_VALUE;
